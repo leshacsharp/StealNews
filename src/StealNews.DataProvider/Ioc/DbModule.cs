37db249c9ee9160c928af2796429ddc5a57038e3
Starchenko Aleksey <star4enko.aleksey2015@yandex.ru>
2019-09-22 13:29:03 +0300

    Added DI for database dependencies

---
+++ src/StealNews.DataProvider/Ioc/DbModule.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using StealNews.DataProvider.Repositories.Abstraction;
+using StealNews.DataProvider.Repositories.Implementation;
+
+namespace StealNews.DataProvider.Ioc
+{
+    public static class DbModule
+    {
+        public static void AddDbDependencies(this IServiceCollection services)
+        {
+            services.AddScoped<INewsRepository, NewsRepository>();
+        }
+    }
+}
