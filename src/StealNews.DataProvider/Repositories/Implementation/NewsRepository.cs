5dd1bf5eab18f741d698e542c8b54674f68a732c
Starchenko Aleksey <star4enko.aleksey2015@yandex.ru>
2019-09-22 13:28:08 +0300

    Added implementation of News repository

---
+++ src/StealNews.DataProvider/Repositories/Implementation/NewsRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Options;
+using StealNews.DataProvider.Repositories.Abstraction;
+using StealNews.DataProvider.Settings;
+using StealNews.Model.Entities;
+
+namespace StealNews.DataProvider.Repositories.Implementation
+{
+    public class NewsRepository : BaseRepository<News>, INewsRepository
+    {
+        public NewsRepository(IOptions<DbSettings> connectionString)
+            : base(connectionString.Value.ConnectionString)
+        {
+
+        }
+    }
+}
