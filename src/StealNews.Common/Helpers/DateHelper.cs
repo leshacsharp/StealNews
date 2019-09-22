868840efb171690a18023eb359df7ff3dea8ced3
Starchenko Aleksey <star4enko.aleksey2015@yandex.ru>
2019-09-22 21:32:38 +0300

    Added date helper

---
+++ src/StealNews.Common/Helpers/DateHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace StealNews.Common.Helpers
+{
+    public static class DateHepler
+    {
+        private static string[] _months = new[] { "янв", "фев", "март", "апр", "ма", "июн", "июл", "авг", "сент", "окт", "ноя", "дек" };
+
+        public static int GetMonthByName(string mounth)
+        {
+            if(mounth == null)
+            {
+                throw new ArgumentNullException(nameof(mounth));
+            }
+
+            var parsedMonth = mounth.Replace(" ", string.Empty).ToLower();
+            var numberOfMounth = 0;
+
+            for (int i = 0; i < _months.Length; i++)
+            {
+                if(parsedMonth.IndexOf(_months[i]) > 0)
+                {
+                    numberOfMounth = i + 1;
+                    break;
+                }
+            }
+
+            if(numberOfMounth == 0)
+            {
+                throw new ArgumentException("The mounth title is invalid", nameof(mounth));
+            }
+
+            return numberOfMounth;
+        }
+    }
+}
